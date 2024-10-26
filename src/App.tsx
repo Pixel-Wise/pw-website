2f34348aec54042a5dff439c2075f177b11f1430
jsalazarv <jsalazarv8@gmail.com>
2024-10-25 18:36:41 -0600

    chore: added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { ExampleProvider } from './domain/example/ExampleContext'
-import ExampleView from './ui/views/ExampleView'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ExampleProvider } from './domain/example/ExampleContext';
+import AppRoutes from './router';
+import './ui/assets/styles/style.css';
 
 const App: React.FC = () => {
   return (
-    <ExampleProvider>
-      <ExampleView />
-    </ExampleProvider>
-  )
-}
+    <Router>
+      <ExampleProvider>
+        <AppRoutes />
+      </ExampleProvider>
+    </Router>
+  );
+};
 
-export default App
+export default App;
